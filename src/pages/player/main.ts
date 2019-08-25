81f0d674d011bad70d7224562558e03231df5298
Aaron Klinker <aaronklinker1@gmail.com>
2019-08-24 20:14:20 -0500

    Cleanup

--- src/pages/player/main.ts
+++ src/pages/player/main.ts
@@ -4,19 +4,20 @@ import store from '../../shared/store';
 import Ripple from 'vue-ripple-directive';
 
 const rootQuery = getRootQuery();
-const parent = document.querySelector(rootQuery) as HTMLElement;
-parent.classList.add('hide-for-anime-skip');
 console.info(`Injecting player onto ${rootQuery}`);
+
+// Set the style to hide all the old elements
+document.body.classList.add('hide-for-anime-skip');
 
 Ripple.color = 'rgba(255, 255, 255, 0.12)';
 Vue.directive('ripple', Ripple);
-
 Vue.config.productionTip = false;
 
 const vue = new Vue({
   store,
   render: h => h(Player),
 }).$mount();
+const parent = document.querySelector(rootQuery) as HTMLElement;
 parent.appendChild(vue.$el);
 
 onVideoChanged(video => {
