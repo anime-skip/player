718f99fe8c3074752291f1e6380a5cf4ea3357ef
Aaron Klinker <aaronklinker1@gmail.com>
2019-08-24 20:04:54 -0500

    Removed old code

--- src/extension/js/services/example.ts
+++ src/extension/js/services/example.ts
@@ -10,10 +10,3 @@ function getRootQuery(): string {
 function getVideoQuery(): string {
   return '#video';
 }
-
-var video = document.querySelector('#video') as HTMLVideoElement;
-
-// @ts-ignore
-function getElementsToHide(): HTMLElement[] {
-  return [document.querySelector('.glass') as HTMLElement];
-}
