c6267b1705c3146ce1be59a957e5eb7064154d7a
Aaron Klinker <aaronklinker1@gmail.com>
2020-07-18 10:40:36 -0500

    Added handler for adding a timestamp

---
+++ src/player/components/EditTimestampHandle.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <div class="EditTimestampHandle" v-ripple>
+      <WebExtImg
+        class="add"
+        src="ic_add_timestamp.svg"
+        :draggable="false"
+        @click.native="pause()"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch, Mixins } from 'vue-property-decorator';
+import WebExtImg from '@/common/components/WebExtImg.vue';
+import VideoControllerMixin from '../../common/mixins/VideoController';
+
+@Component({
+  components: { WebExtImg },
+})
+export default class EditTimestampHandle extends Mixins(VideoControllerMixin) {
+  createNewTimestamp(): void {
+    this.pause();
+    const newTimestamp: Api.InputTimestamp = {
+      at: this.getCurrentTime(),
+      typeId: 0,
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  bottom: 12px;
+  width: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow-x: visible;
+}
+.EditTimestampHandle {
+  min-width: 40px;
+  min-height: 40px;
+  background-color: $divider;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 20px;
+  transition: 250ms;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.18);
+  }
+}
+</style>
