6a39dd09d8b6b3c8a00d3b5a2088dbdb30ffac84
Aaron Klinker <aaronklinker1@gmail.com>
2020-08-06 22:22:40 -0500

    Updated episode numbering logic

---
+++ src/common/utils/__tests__/EpisodeUtils.test.ts
@@ -0,0 +1,83 @@
+import EpisodeUtils from '../EpisodeUtils';
+
+describe('EpisodeUtils', () => {
+  describe('seasonAndNumberFromSearchResult', () => {
+    it('should return all the values when they are passed', () => {
+      const input = {
+        season: 0,
+        number: 0,
+        absoluteNumber: 0,
+      } as Api.EpisodeSearchResult;
+      const expected = 'Season 0, Episode 0 (#0)';
+
+      const actual = EpisodeUtils.seasonAndNumberFromSearchResult(input);
+
+      expect(actual).toBe(expected);
+    });
+
+    it("should return the season and number when only the absolute number isn't passed", () => {
+      const input = {
+        season: 0,
+        number: 0,
+        absoluteNumber: undefined,
+      } as Api.EpisodeSearchResult;
+      const expected = 'Season 0, Episode 0';
+
+      const actual = EpisodeUtils.seasonAndNumberFromSearchResult(input);
+
+      expect(actual).toBe(expected);
+    });
+
+    it("should return the season and absolute number when the number isn't passed", () => {
+      const input = {
+        season: 0,
+        number: undefined,
+        absoluteNumber: 0,
+      } as Api.EpisodeSearchResult;
+      const expected = 'Season 0 (#0)';
+
+      const actual = EpisodeUtils.seasonAndNumberFromSearchResult(input);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('should return the season when it is the only thing passed', () => {
+      const input = {
+        season: 0,
+        number: undefined,
+        absoluteNumber: undefined,
+      } as Api.EpisodeSearchResult;
+      const expected = 'Season 0';
+
+      const actual = EpisodeUtils.seasonAndNumberFromSearchResult(input);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('should return the number when it is the only thing passed', () => {
+      const input = {
+        season: undefined,
+        number: 0,
+        absoluteNumber: undefined,
+      } as Api.EpisodeSearchResult;
+      const expected = 'Episode 0';
+
+      const actual = EpisodeUtils.seasonAndNumberFromSearchResult(input);
+
+      expect(actual).toBe(expected);
+    });
+
+    it('should return the absolute number when it is the only thing passed', () => {
+      const input = {
+        season: undefined,
+        number: undefined,
+        absoluteNumber: 0,
+      } as Api.EpisodeSearchResult;
+      const expected = '#0';
+
+      const actual = EpisodeUtils.seasonAndNumberFromSearchResult(input);
+
+      expect(actual).toBe(expected);
+    });
+  });
+});
