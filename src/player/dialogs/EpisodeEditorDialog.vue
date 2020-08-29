ba755d3f9780a5c6a39b28ad04a13d160ce278e1
Aaron Klinker <aaronklinker1@gmail.com>
2020-08-29 14:45:51 -0500

    Refactored edit episode to be smaller

--- src/player/dialogs/EpisodeEditorDialog.vue
+++ src/player/dialogs/EpisodeEditorDialog.vue
@@ -6,97 +6,52 @@
     @show="onShowDialog()"
   >
     <ProgressOverlay :isLoading="isLoadingEpisode">
-      <h2 class="section-header">Find Existing Epiosde</h2>
+      <h2 class="section-header">Find Existing Episode</h2>
       <AutocompleteTextInput
         class="row"
         label="Find Show"
-        v-model="showSearch"
-        :isValid="isShowSelected()"
-        isShowingSuggestion
-        @focus="onFocusShowSearch(true)"
-        @blur="onFocusShowSearch(false)"
-      >
-        <div
-          v-for="(show, index) in showSearchListItems"
-          :key="show.id"
-          class="item"
-          @mouseup="onClickShowResult(show.id, index)"
-        >
-          <p class="title">{{ show.title }}</p>
-          <p v-if="show.subtitle" class="subtitle">
-            {{ show.subtitle }}
-          </p>
-        </div>
-      </AutocompleteTextInput>
+        v-model="selectedShowOption"
+        :options="showSearchListItems"
+        @select="onChangeSelectedShow"
+        @search="searchShows"
+      />
       <AutocompleteTextInput
-        v-if="isExistingShowSelected()"
         class="row"
         label="Find Episode"
-        v-model="episodeSearch"
-        :isValid="isEpisodeSelected()"
-        isShowingSuggestions
-        @focus="onFocusEpisodeSearch(true)"
-        @blur="onFocusEpisodeSearch(false)"
-      >
-        <div
-          v-for="(epiosde, index) in episodeSearchListItems"
-          :key="epiosde.id"
-          class="item"
-          @mouseup="onClickEpisodeResult(epiosde.id, index)"
-        >
-          <p class="title">{{ epiosde.title }}</p>
-          <p v-if="epiosde.subtitle" class="subtitle">
-            {{ epiosde.subtitle }}
-          </p>
-        </div>
-      </AutocompleteTextInput>
-
-      <div class="edit-section" v-if="isShowingEditSection()">
-        <h2 class="section-header pre-section-spacing">Edit Episode Info</h2>
-        <p v-if="isTempEditingDisabled || selectedShowId !== 'new-show-id'" class="disabled-label">
-          (Editing existing data is not supported yet)
-        </p>
+        v-model="selectedEpisodeOption"
+        :options="episodeSearchListItems"
+        @select="onChangeSelectedEpisode"
+        @search="onSearchEpisodes"
+      />
+      <div class="row flex-row">
         <TextInput
-          class="row"
-          label="Show Name"
-          v-model="editableShowName"
-          :disabled="isTempEditingDisabled || selectedShowId !== 'new-show-id'"
+          class="flex-1"
+          label="Season"
+          v-model="editableSeasonNumber"
+          :disabled="shouldDisableEpisodeNumbers"
         />
+        <div class="column-space" />
         <TextInput
-          class="row"
-          label="Episode Name"
-          v-model="editableEpisodeName"
-          :disabled="isTempEditingDisabled"
+          class="flex-1"
+          label="Episode #"
+          v-model="editableEpisodeNumber"
+          :disabled="shouldDisableEpisodeNumbers"
         />
-        <div class="row flex-row">
-          <TextInput
-            class="flex-1"
-            label="Season"
-            v-model="editableSeasonNumber"
-            :disabled="isTempEditingDisabled"
-          />
-          <div class="column-space" />
-          <TextInput
-            class="flex-1"
-            label="Episode #"
-            v-model="editableEpisodeNumber"
-            :disabled="isTempEditingDisabled"
-          />
-          <div class="column-space" />
-          <TextInput
-            class="flex-1"
-            label="Overall #"
-            v-model="editableAbsoluteNumber"
-            :disabled="isTempEditingDisabled"
-          />
-        </div>
-        <input
-          type="submit"
-          value="Save Episode"
-          class="clickable focus button save"
-          @click="onClickSaveChanges"
+        <div class="column-space" />
+        <TextInput
+          class="flex-1"
+          label="Overall #"
+          v-model="editableAbsoluteNumber"
+          :disabled="shouldDisableEpisodeNumbers"
         />
       </div>
+      <input
+        type="submit"
+        value="Save Episode"
+        class="clickable focus button save"
+        :disabled="true"
+        @click="onClickSaveChanges"
+      />
     </ProgressOverlay>
   </BasicDialog>
 </template>
@@ -114,17 +69,6 @@ import AutocompleteTextInput from '@/common/components/AutocompleteTextInput.vue
 import EpisodeUtils from '../../common/utils/EpisodeUtils';
 import Browser from '../../common/utils/Browser';
 
-const CREATE_NEW_SHOW_ID = 'new-show-id';
-const CREATE_NEW_SHOW_TITLE = '+ New show';
-const CREATE_NEW_EPISODE_ID = 'new-episode-id';
-const CREATE_NEW_EPISODE_TITLE = '+ New episode';
-
-interface AutocompleteItem {
-  id: string;
-  title: string;
-  subtitle?: string;
-}
-
 @Component({
   components: { BasicDialog, PopupHeader, ProgressOverlay, TextInput, AutocompleteTextInput },
 })
@@ -136,192 +80,116 @@ export default class EpisodeEditorDialog extends Vue {
   @Getter() tabUrl!: string;
 
   @Mutation('searchShowsResult') clearShowSearchResults!: () => void;
-  @Mutation('searchEpisodesResult') clearEpisodeSearchResults!: () => void;
+  @Mutation('searchEpisodesResult') clearSearchEpisodeResults!: () => void;
 
   @Action() searchShows!: (name: string) => void;
   @Action() searchEpisodes!: (payload: { name: string; showId?: string }) => void;
   @Action() createEpisodeData!: (payload: CreateEpisodeDataPayload) => void;
   @Action() showDialog!: (dialog?: string) => void;
 
-  public showSearch: string = '';
-  public showSearchTimeout: any = undefined;
-  public selectedShowId: string | undefined = undefined;
+  public selectedShowOption: any = {
+    title: '',
+  };
+  public selectedEpisodeOption: any = {
+    title: '',
+  };
 
-  public episodeSearch: string = '';
-  public episodeSearchTimeout: any = undefined;
-  public selectedEpisodeId: string | undefined = undefined;
-
-  public editableShowName = '';
-  public editableEpisodeName = '';
   public editableSeasonNumber = '';
   public editableEpisodeNumber = '';
   public editableAbsoluteNumber = '';
 
+  public onChangeSelectedShow() {
+    this.selectedEpisodeOption = {
+      title: '',
+    };
+    this.$forceUpdate();
+    this.onSearchEpisodes('');
+    this.onChangeSelectedEpisode(this.selectedEpisodeOption);
+  }
+
+  public onChangeSelectedEpisode(currentValue: any) {
+    console.log('[EpisodeEditorDialog] onChangeSelectedEpisode');
+    const episode = this.searchEpisodesResult.find(result => result.id === currentValue.id);
+    this.editableSeasonNumber = String(episode?.season ?? '');
+    this.editableEpisodeNumber = String(episode?.number ?? '');
+    this.editableAbsoluteNumber = String(episode?.absoluteNumber ?? '');
+  }
+
   public onShowDialog() {
-    const episodeUrl = this.episodeUrl;
-    this.showSearch = episodeUrl?.episode.show?.name ?? '';
-    this.selectedShowId = episodeUrl?.episode.show?.id;
-    this.episodeSearch = episodeUrl?.episode.name ?? '';
-    this.selectedEpisodeId = episodeUrl?.episode.id;
-    this.editableShowName = this.showSearch;
-    this.editableEpisodeName = this.episodeSearch;
-    this.editableSeasonNumber = String(episodeUrl?.episode.season ?? '');
-    this.editableEpisodeNumber = String(episodeUrl?.episode.number ?? '');
-    this.editableAbsoluteNumber = String(episodeUrl?.episode.absoluteNumber ?? '');
-  }
-
-  public get headerTitle() {
-    if (this.isLoadingEpisode) {
-      return 'Episode';
-    }
-    if (
-      this.selectedEpisodeId == CREATE_NEW_EPISODE_ID ||
-      this.selectedEpisodeId == CREATE_NEW_SHOW_ID
-    ) {
-      return 'Create Episode';
-    }
-    return `Edit Episode`;
-  }
-
-  public get isTempEditingDisabled(): boolean {
-    return (
-      this.selectedShowId !== CREATE_NEW_SHOW_ID && this.selectedEpisodeId !== CREATE_NEW_EPISODE_ID
-    );
-  }
-
-  public get showSearchListItems(): AutocompleteItem[] {
-    return [
-      ...this.searchShowsResult.map(item => ({
-        id: item.id,
-        title: item.name,
-        subtitle: item.originalName,
-      })),
-      {
-        id: CREATE_NEW_SHOW_ID,
-        title: CREATE_NEW_SHOW_TITLE,
-      },
-    ];
-  }
-
-  public get episodeSearchListItems(): AutocompleteItem[] {
-    return [
-      ...this.searchEpisodesResult.map(item => ({
-        id: item.id,
-        title: item.name || '(No title)',
-        subtitle: EpisodeUtils.seasonAndNumberFromSearchResult(item),
-      })),
-      {
-        id: CREATE_NEW_EPISODE_ID,
-        title: CREATE_NEW_EPISODE_TITLE,
-      },
-    ];
-  }
-
-  @Watch('showSearch')
-  public onChangeShowSearch(showSearch: string) {
-    if (this.showSearchTimeout) {
-      clearTimeout(this.showSearchTimeout);
-    }
-    this.showSearchTimeout = setTimeout(() => {
-      this.searchShows(this.showSearch.trim());
-    }, 500);
-  }
-
-  public onFocusShowSearch(isFocused: boolean) {
-    if (isFocused && this.selectedShowId === CREATE_NEW_SHOW_ID) {
-      this.showSearch = '';
-      this.selectedShowId = undefined;
-    }
-    if (!isFocused) {
-      const selectedShowId = this.showSearchListItems.find(item => item.title === this.showSearch)
-        ?.id;
-      if (selectedShowId !== this.selectedShowId) {
-        this.selectedShowId = selectedShowId;
-        this.episodeSearch = '';
-        this.selectedEpisodeId = undefined;
+    if (this.episodeUrl) {
+      if (this.episodeUrl?.episode.show) {
+        this.selectedShowOption = {
+          id: this.episodeUrl.episode.show.id,
+          title: this.episodeUrl.episode.show.name,
+          subtitle: this.episodeUrl.episode.show.originalName,
+        };
       }
+      console.log('Episode', JSON.parse(JSON.stringify(this.episodeUrl)));
+      this.selectedEpisodeOption = {
+        id: this.episodeUrl.episode.id,
+        title: this.episodeUrl.episode.name ?? '',
+        subtitle: EpisodeUtils.seasonAndNumberFromEpisodeUrl(this.episodeUrl),
+      };
+    } else {
+      this.selectedShowOption = {
+        title: '',
+      };
+      this.selectedEpisodeOption = {
+        title: '',
+      };
     }
+    this.editableSeasonNumber = String(this.episodeUrl?.episode.season ?? '');
+    this.editableEpisodeNumber = String(this.episodeUrl?.episode.number ?? '');
+    this.editableAbsoluteNumber = String(this.episodeUrl?.episode.absoluteNumber ?? '');
   }
 
-  @Watch('episodeSearch')
-  public onChangeEpisodeSearch(episodeSearch: string) {
-    if (this.episodeSearchTimeout) {
-      clearTimeout(this.episodeSearchTimeout);
+  public get showSearchListItems() {
+    return this.searchShowsResult.map(item => ({
+      id: item.id,
+      title: item.name,
+      subtitle: item.originalName,
+    }));
+  }
+
+  public get episodeSearchListItems() {
+    return this.searchEpisodesResult.map(item => ({
+      id: item.id,
+      title: item.name || '(No title)',
+      subtitle: EpisodeUtils.seasonAndNumberFromSearchResult(item),
+    }));
+  }
+
+  public get shouldDisableEpisodeNumbers(): boolean {
+    return this.selectedEpisodeOption.id != null || !this.selectedEpisodeOption.title;
+  }
+
+  public onSearchEpisodes(episodeNameSearch: string): void {
+    if (this.selectedShowOption.id == null) {
+      this.clearSearchEpisodeResults();
+      return;
     }
-    this.episodeSearchTimeout = setTimeout(() => {
-      this.searchEpisodes({ name: this.episodeSearch.trim(), showId: this.selectedShowId });
-    }, 500);
-  }
 
-  public onFocusEpisodeSearch(isFocused: boolean) {
-    if (isFocused && this.episodeSearch === CREATE_NEW_EPISODE_TITLE) {
-      this.episodeSearch = '';
-      this.selectedEpisodeId = undefined;
-    }
-    if (!isFocused) {
-      const episode = this.episodeSearchListItems.find(item => item.title === this.episodeSearch);
-      this.selectedEpisodeId = episode?.id;
-    }
-  }
-
-  public isExistingShowSelected(): boolean {
-    return this.isShowSelected() && !this.isCreatingNewShow();
-  }
-  public isShowSelected(): boolean {
-    return this.selectedShowId != null;
-  }
-  public isCreatingNewShow(): boolean {
-    return this.selectedShowId === CREATE_NEW_SHOW_ID;
-  }
-
-  public isEpisodeSelected(): boolean {
-    return this.selectedEpisodeId != null;
-  }
-  public isCreatingNewEpisode(): boolean {
-    return this.selectedEpisodeId === CREATE_NEW_EPISODE_ID;
-  }
-
-  public isShowingEditSection(): boolean {
-    return this.isCreatingNewShow() || (this.isShowSelected() && this.isEpisodeSelected());
+    this.searchEpisodes({
+      showId: this.selectedShowOption.id,
+      name: episodeNameSearch,
+    });
   }
 
   public get isLoadingEpisode(): boolean {
-    const requestState = this.episodeRequestState;
-    return requestState === RequestState.LOADING || requestState === RequestState.NOT_REQUESTED;
-  }
-
-  public onClickShowResult(id: string, index: number) {
-    const item = this.showSearchListItems[index];
-    this.selectedShowId = id;
-    this.showSearch = item.title;
-    if (id !== CREATE_NEW_SHOW_ID) {
-      this.editableShowName = item.title;
-    }
-    (document.activeElement as any).blur();
-  }
-
-  public onClickEpisodeResult(id: string, index: number) {
-    const item = this.episodeSearchListItems[index];
-    const fullItem = this.searchEpisodesResult.find(i => i.id === item?.id);
-    this.selectedEpisodeId = id;
-    this.episodeSearch = item.title;
-    if (fullItem) {
-      this.editableEpisodeName = fullItem.name ?? '';
-      this.editableEpisodeNumber = String(fullItem.number ?? '');
-      this.editableSeasonNumber = String(fullItem.season ?? '');
-      this.editableAbsoluteNumber = String(fullItem.absoluteNumber ?? '');
-    }
-    (document.activeElement as any).blur();
+    return (
+      this.episodeRequestState === RequestState.LOADING ||
+      this.episodeRequestState === RequestState.NOT_REQUESTED
+    );
   }
 
   public isShowChanged(): boolean {
-    return this.editableShowName.trim() !== this.episodeUrl?.episode.show?.name;
+    return this.selectedShowOption.title.trim() !== this.episodeUrl?.episode.show?.name;
   }
 
   public isEpisodeChanged(): boolean {
     const existingEpisode = this.episodeUrl?.episode;
     return (
-      this.editableEpisodeName.trim() !== existingEpisode?.name ||
+      this.selectedEpisodeOption.title.trim() !== existingEpisode?.name ||
       this.editableEpisodeNumber.trim() !==
         (existingEpisode?.number ? String(existingEpisode.number) : undefined) ||
       this.editableSeasonNumber.trim() !==
@@ -333,7 +201,7 @@ export default class EpisodeEditorDialog extends Vue {
 
   public onClickSaveChanges() {
     // Prepare data
-    const episodeName = this.editableEpisodeName;
+    const episodeName = this.selectedEpisodeOption.title;
     const number = isFinite(Number(this.editableEpisodeNumber))
       ? Number(this.editableEpisodeNumber)
       : undefined;
@@ -351,11 +219,11 @@ export default class EpisodeEditorDialog extends Vue {
     };
 
     // Creating Data
-    if (this.selectedShowId === CREATE_NEW_SHOW_ID) {
+    if (this.selectedShowOption.id === null) {
       this.createEpisodeData({
         show: {
           create: true,
-          name: this.editableShowName,
+          name: this.selectedShowOption.title,
         },
         episode: {
           create: true,
@@ -367,11 +235,11 @@ export default class EpisodeEditorDialog extends Vue {
         },
       });
     }
-    if (this.selectedEpisodeId === CREATE_NEW_EPISODE_ID) {
+    if (this.selectedEpisodeOption.id === null) {
       this.createEpisodeData({
         show: {
           create: false,
-          showId: this.selectedShowId!,
+          showId: this.selectedShowOption.id,
         },
         episode: {
           create: true,
@@ -385,18 +253,18 @@ export default class EpisodeEditorDialog extends Vue {
       return;
     }
     if (
-      this.selectedEpisodeId !== CREATE_NEW_EPISODE_ID &&
-      this.selectedShowId !== CREATE_NEW_SHOW_ID &&
+      this.selectedEpisodeOption.id != null &&
+      this.selectedShowOption.id != null &&
       this.episodeUrl == null
     ) {
       this.createEpisodeData({
         show: {
           create: false,
-          showId: this.selectedShowId!,
+          showId: this.selectedShowOption.id,
         },
         episode: {
           create: false,
-          episodeId: this.selectedEpisodeId!,
+          episodeId: this.selectedEpisodeOption.id!,
         },
         episodeUrl: {
           create: true,
@@ -407,11 +275,11 @@ export default class EpisodeEditorDialog extends Vue {
     }
 
     // Change relationships
-    if (this.episodeUrl?.episode?.show?.id !== this.selectedShowId) {
+    if (this.episodeUrl?.episode.show?.id !== this.selectedShowOption.id) {
       console.log('Update episode.showId');
       return;
     }
-    if (this.episodeUrl?.episode.id !== this.selectedEpisodeId) {
+    if (this.episodeUrl?.episode.id !== this.selectedEpisodeOption.id) {
       console.log('Update episodeUrl.episodeId');
       return;
     }
@@ -449,6 +317,8 @@ $borderRadius: 3px;
     overflow-y: auto;
     max-height: 70%;
     width: $width;
+    overflow-y: visible;
+
     & > * {
       padding: 14px 16px;
     }
@@ -458,49 +328,9 @@ $borderRadius: 3px;
     margin-top: 0px;
   }
 
-  .pre-section-spacing {
-    margin-top: 24px;
-  }
-
-  .item {
-    height: 48px;
-    border-top: 1px solid $divider;
-    padding: 0 16px;
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    cursor: pointer;
-    transition: 200ms;
-
-    &:hover {
-      background-color: $divider;
-      border-top-color: transparent;
-    }
-
-    .title {
-      padding-top: 2px;
-      font-size: 15px;
-      color: $textPrimary;
-    }
-
-    .subtitle {
-      color: $textSecondary;
-      margin-top: -1px;
-      font-size: 13px;
-    }
-  }
-
   .edit-section {
     display: flex;
     flex-direction: column;
-
-    .disabled-label {
-      margin-top: 8px;
-      font-size: 14px;
-      color: rgba($color: #ff7777, $alpha: 0.7);
-    }
   }
 
   .row {
