9f2103f1470596523c013ef690ef81d5c21ffb1c
Aaron Klinker <aaronklinker1@gmail.com>
2020-09-13 22:49:29 -0500

    Setup funimation content scripts
    - Parent got a messenger to listen for inferred episode info requests
    - index got the new inferEpisodeInfo function

--- src/content-scripts/funimation/parent.ts
+++ src/content-scripts/funimation/parent.ts
@@ -1,1 +1,30 @@
 console.log('INJECTED content-scripts/funimation/parent.ts');
+
+import Messenger from '../../common/utils/Messenger';
+
+new Messenger<
+  ParentMessageTypes,
+  ParentMessageListenerMap,
+  ParentMessagePayloadMap,
+  ParentMessageResponseMap
+>('funimation parent', {
+  '@anime-skip/inferEpisodeInfo': async (): Promise<InferredEpisodeInfo> => {
+    const show = document.querySelector('.video-title a')?.textContent ?? undefined;
+    const number = document
+      .querySelector('.video-title')
+      ?.textContent?.replace(show || '', '')
+      ?.replace('Episode', '')
+      ?.trim();
+    const name = document.querySelector('h2.episode-headline')?.textContent ?? undefined;
+    console.log('parent.inferEpisodeInfo', {
+      show,
+      number,
+      name,
+    });
+    return {
+      show,
+      number,
+      name,
+    };
+  },
+});
