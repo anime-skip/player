328a47e030039a46046eb0a22f3122cde2fccb2e
Aaron Klinker <aaronklinker1@gmail.com>
2020-09-15 22:03:51 -0500

    Setup packaging with the chrome split

--- scripts/package.sh
+++ scripts/package.sh
@@ -35,7 +35,9 @@ onFailure() {
 }
 trap 'onFailure' ERR
 
+
 # SETUP ################################
+
 PACKAGE_VERSION=$(node -e 'console.log(require("./package.json").version)')
 PACKAGE_NAME=$(node -e 'console.log(require("./package.json").name)')
 OUTPUT_DIRECTORY="artifacts"
@@ -53,39 +55,64 @@ echo -e "  ${DIM}LOG_DIRECTORY:    ${RESET}${BOLD}$LOG_DIRECTORY${RESET}"
 echo -e "  ${DIM}LOG_FILE:         ${RESET}${BOLD}$LOG_FILE${RESET}"
 echo -e "  ${DIM}OUTPUT_NAME:      ${RESET}${BOLD}$OUTPUT_NAME${RESET}"
 
+
 # CLEANUP ##############################
+
 title "Prepare Output Directories"
-mkdir -p "$LOG_DIRECTORY"
+mkdir -p "$LOG_DIRECTORY" # Don't use 'run' since it depends on this directory
 
 run "rm ${CODE}dist"            "rm -rf 'dist'"
 run "rm ${CODE}$OUTPUT_NAME/*"  "rm -rf '$OUTPUT_NAME'"
 run "mkdir ${CODE}$OUTPUT_NAME" "mkdir -p '$OUTPUT_NAME'"
 
+
 # PREPACKAGE ###########################
+
 title "Pre-package"
-run "Install dependencies" "yarn install"
-run "Build ${CODE}dist/"   "PACKAGING=true yarn build:prod"
-run "Verify manifest"      "yarn web-ext --config=.web-ext.config.js lint"
+
+run "Install dependencies"  "yarn install"
+run "Check for type errors" "yarn compile"
+run "Check formatting"      "yarn prettier"
+run "Lint"                  "yarn lint"
+run "Run Tests"             "yarn test"
+run "Run Integration Tests" "yarn test:integration"
+run "Run E2E Tests"         "yarn test:e2e"
+run "Verify manifest"       "yarn check-manifest"
+
 
 # PACKAGE ##############################
+
 title "Packaging Artifacts"
-run "Create ${CODE}extension.zip" "yarn web-ext --config=.web-ext.config.js build -a '$OUTPUT_NAME' -n 'extension.zip'"
-# TODO Create Firefox Sources
+
+# All
+# TODO - Create source.zip
+
+# Firefox
+run "Building for ${CODE}firefox" "yarn build:prod"
+run "Create ${CODE}firefox.zip" "(cd dist && zip -r '$OUTPUT_NAME/firefox.zip' .)"
+
 # Chrome
-run "Create ${CODE}chrome.zip" "zip -r '$OUTPUT_NAME/chrome.zip' dist/*"
-# TODO - Edge & Opera
+run "Building for ${CODE}chrome" "yarn build:chrome:prod"
+run "Create ${CODE}chrome.zip" "(cd dist && zip -r '$OUTPUT_NAME/chrome.zip' .)"
+
 
 # DEPLOY ###############################
+
 title "Deploying"
+
 # Firefox
 run "Signing and uploading ${CODE}firefox.xpi" "yarn web-ext --config=.web-ext.config.js sign -a '$OUTPUT_NAME' -n 'firefox.xpi' --api-key '$FIREFOX_SIGNING_ISSUER' --api-secret '$FIREFOX_SIGNING_SECRET' --id='$FIREFOX_SIGNING_ID'"
 mv "$OUTPUT_NAME/anime_skip-$PACKAGE_VERSION-an+fx.xpi" "$OUTPUT_NAME/firefox.xpi"
+
 # Chrome
 run "Submit ${CODE}chrome.zip${RESET} for review" "./scripts/chrome-publish.sh '$OUTPUT_NAME/chrome.zip' 2>&1"
+
+# All
 run "Create and push ${CODE}v$PACKAGE_VERSION${RESET} tag" "git tag 'v$PACKAGE_VERSION' && git push --tags &> /dev/null"
-# TODO
+
 
 # CLEANUP ##############################
+
 echo ""
 echo -e "${BOLD}${GREEN}Done!${RESET}"
 echo ""
