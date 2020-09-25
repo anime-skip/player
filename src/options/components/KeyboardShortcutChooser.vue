957ccfca7bf3b92fd03f081302cb8cad9bf23d33
Aaron Klinker <aaronklinker1@gmail.com>
2020-09-25 18:23:31 -0500

    Support large key combos better for the smaller width

--- src/options/components/KeyboardShortcutChooser.vue
+++ src/options/components/KeyboardShortcutChooser.vue
@@ -86,6 +86,8 @@ export default Vue.extend({
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 0 4px;
+
   & > pre {
     font-family: monospace;
     font-size: 16px;
