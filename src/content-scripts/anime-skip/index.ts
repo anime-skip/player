e3d3e96238bc77a806b992c67df15dfd05873210
Aaron Klinker <aaronklinker1@gmail.com>
2020-10-10 21:49:01 -0500

    Custom event's to interact with the extension

--- src/content-scripts/anime-skip/index.ts
+++ src/content-scripts/anime-skip/index.ts
@@ -1,6 +1,9 @@
 import Browser from '@/common/utils/Browser';
+import Messenger from '@/common/utils/Messenger';
 
 console.log('INJECTED content-scripts/anime-skip/index.ts');
+
+const messenger = new Messenger('anime-skip.com integration');
 
 setInterval(() => {
   // Install check
@@ -11,3 +14,10 @@ setInterval(() => {
     if (token) window.postMessage('@anime-skip/login-check', '*');
   });
 }, 1000);
+
+document.addEventListener('@anime-skip/open-popup', function() {
+  messenger.send('@anime-skip/open-popup', undefined);
+});
+document.addEventListener('@anime-skip/open-options', function() {
+  messenger.send('@anime-skip/open-options', undefined);
+});
