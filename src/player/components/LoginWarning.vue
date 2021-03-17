ac13db0d8634737038a806daf1cede8a1e2b6f0d
Aaron Klinker <aaronklinker1@gmail.com>
2021-03-17 11:16:18 -0500

    feat: Added reusable login warning

---
+++ src/player/components/LoginWarning.vue
@@ -0,0 +1,27 @@
+<template>
+  <p class="text-error">
+    You need to be
+    <a href="#" @click.prevent.stop="openLoginDialog" class="underline">logged in</a>
+    before {{ before }}
+  </p>
+</template>
+
+<script>
+import useLoginDialog from '@/common/composition/useLoginDialog';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    before: String,
+  },
+  setup(props) {
+    const { openLoginDialog } = useLoginDialog();
+    return {
+      openLoginDialog,
+      before: props.before,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
