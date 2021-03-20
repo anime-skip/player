467ba92794087fd81b77a4131ddef6777c461082
Aaron Klinker <aaronklinker1@gmail.com>
2021-03-19 19:32:48 -0500

    fix: Make the timestamp icons the same color as the progress bar

--- src/player/utils/TimelineColors.ts
+++ src/player/utils/TimelineColors.ts
@@ -1,7 +1,7 @@
 import { theme } from '@anime-skip/ui';
 
 export default {
-  default: theme['backgroundColor-primaryPalette-400'],
+  default: theme['backgroundColor-primaryPalette-500'],
   defaultLight: theme['fill-primary'],
   new: theme['fill-success'],
   edited: theme['backgroundColor-secondaryPalette-300'],
