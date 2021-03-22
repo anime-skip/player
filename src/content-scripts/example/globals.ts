781c4fa2ffa5dd10c56f4a4018260a610e0ac9a2
Aaron Klinker <aaronklinker1@gmail.com>
2021-03-21 23:12:26 -0500

    chore: Refactored all service globals setup to use the new, standard `setupGlobals()`

--- src/content-scripts/example/globals.ts
+++ src/content-scripts/example/globals.ts
@@ -1,20 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import setupGlobals from '../setupGlobals';
 import './style.scss';
 
-console.log('INJECTED content-scripts/example/index.ts');
-
-global.service = 'example';
-global.serviceDisplayName = 'Anime Skip Example';
-
-global.getRootQuery = (): string => {
-  return '.video-container';
-};
-
-global.inferEpisodeInfo = async (): Promise<InferredEpisodeInfo> => {
-  console.debug('example.inferEpisodeInfo');
-  return await browser.runtime.sendMessage({ type: '@anime-skip/inferEpisodeInfo' });
-};
-
-global.getVideoQuery = (): string => {
-  return '#video';
-};
+setupGlobals('example', {
+  serviceDisplayName: 'Anime Skip Example',
+  getRootQuery() {
+    return '.video-container';
+  },
+  getVideoQuery() {
+    return '#video';
+  },
+});
