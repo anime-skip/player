b68b64ab9492d09f7f74b117af0fa678a3bd3476
Aaron Klinker <aaronklinker1@gmail.com>
2021-04-28 22:40:05 -0500

    fix: Funimation's new parent webpage

--- src/content-scripts/funimation/parent.ts
+++ src/content-scripts/funimation/parent.ts
@@ -2,17 +2,37 @@ import setupParent from '../setupParent';
 
 setupParent('funimation', {
   getEpisodeInfo() {
-    const show = document.querySelector('.video-title a')?.textContent ?? undefined;
-    const number = document
-      .querySelector('.video-title')
-      ?.textContent?.replace(show || '', '')
-      ?.replace('Episode', '')
-      ?.trim();
-    const name = document.querySelector('h2.episode-headline')?.textContent ?? undefined;
-    return {
-      show,
-      number,
-      name,
-    };
+    // X - 2021/05/28
+    if (document.querySelector('.video-title') != null) {
+      const show = document.querySelector('.video-title a')?.textContent ?? undefined;
+      const number = document
+        .querySelector('.video-title')
+        ?.textContent?.replace(show || '', '')
+        ?.replace('Episode', '')
+        ?.trim();
+      const name = document.querySelector('h2.episode-headline')?.textContent ?? undefined;
+      return {
+        show,
+        number,
+        name,
+      };
+    }
+
+    // 2021/05/28 - Current
+    if (document.querySelector('.show-title') != null) {
+      const show = document.querySelector('.show-title a')?.textContent ?? undefined;
+      const number = document
+        .querySelector('.show-title nobr')
+        ?.textContent?.replace('Episode', '')
+        ?.trim();
+      const name = document.querySelector('.v-card__subtitle')?.textContent ?? undefined;
+      return {
+        show,
+        number,
+        name,
+      };
+    }
+
+    return {};
   },
 });
