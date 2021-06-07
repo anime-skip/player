dbfdea8b51532c5f1cab3850c93631335eb6ce12
Aaron Klinker <aaronklinker1@gmail.com>
2021-06-07 16:39:49 -0500

    chore: strip out `MessengerApi`

--- src/background/index.ts
+++ src/background/index.ts
@@ -10,36 +10,13 @@ global.Api = AxiosApi;
 // Setup Messaging
 
 const messenger = new Messenger<
-  ApiMessageTypes & RuntimeMessageTypes,
-  ApiMessageListenerMap & RuntimeMessageListenerMap,
-  ApiMessagePayloadMap & RuntimeMessagePayloadMap,
-  ApiMessageResponseMap & RuntimeMessageResponseMap
+  RuntimeMessageTypes,
+  RuntimeMessageListenerMap,
+  RuntimeMessagePayloadMap,
+  RuntimeMessageResponseMap
 >(
   'background',
   {
-    loginManual: ({ username, password }) => global.Api.loginManual(username, password),
-    loginRefresh: global.Api.loginRefresh,
-
-    updatePreferences: global.Api.updatePreferences,
-
-    createShow: global.Api.createShow,
-    searchShows: global.Api.searchShows,
-
-    createEpisode: ({ data, showId }) => global.Api.createEpisode(data, showId),
-    searchEpisodes: ({ name, showId }) => global.Api.searchEpisodes(name, showId),
-    updateEpisode: ({ episodeId, newEpisode }) => global.Api.updateEpisode(episodeId, newEpisode),
-
-    createEpisodeUrl: ({ data, episodeId }) => global.Api.createEpisodeUrl(data, episodeId),
-    deleteEpisodeUrl: global.Api.deleteEpisodeUrl,
-    fetchEpisodeByUrl: global.Api.fetchEpisodeByUrl,
-    fetchEpisodeByName: ({ name, showName }) => global.Api.fetchEpisodeByName(name, showName),
-    updateEpisodeUrl: ({ episodeUrl, newEpisodeUrl }) =>
-      global.Api.updateEpisodeUrl(episodeUrl, newEpisodeUrl),
-
-    createTimestamp: ({ episodeId, data }) => global.Api.createTimestamp(episodeId, data),
-    updateTimestamp: global.Api.updateTimestamp,
-    deleteTimestamp: global.Api.deleteTimestamp,
-
     '@anime-skip/open-all-settings': async () => {
       await browser.runtime.openOptionsPage();
     },
