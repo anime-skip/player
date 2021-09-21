30ab83125a010c86050ecf24e9432ba6538966cb
Aaron Klinker <aaronklinker1@gmail.com>
2021-09-20 23:31:41 -0500

    chore: simplify new release pre-commit formatting
    The husky pre-commit that does formatting wasn't marked as executable, so it got skipped. Marked it as executable, so we shouldn't need the custom pre-commit script anymore

--- .github/pre-commit.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const { execSync } = require('child_process');
-
-exports.preCommit = async () => {
-  const projectRoot = path.resolve(__dirname, '..');
-  execSync('pnpm format', { cwd: projectRoot });
-};
