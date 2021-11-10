1df487f44970d17fb7a603a49382611a2da03753
Aaron Klinker <aaronklinker1@gmail.com>
2021-11-09 23:35:09 -0600

    fix tooling problems

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ const { pathsToModuleNameMapper } = require('ts-jest/utils');
 const { compilerOptions } = require('./tsconfig');
 
 // Respect tsconfig paths
-const tsconfigPathsModuleNameMapper = pathsToModuleNameMapper(compilerOptions.paths);
-const tsconfigModulePaths = ['<rootDir>'];
+const tsconfigPathsModuleNameMapper = pathsToModuleNameMapper(compilerOptions.paths, {
+  prefix: '<rootDir>/',
+});
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
@@ -13,5 +14,4 @@ module.exports = {
     ...tsconfigPathsModuleNameMapper,
     'webextension-polyfill': '<rootDir>/src/__mocks__/webextension-polyfill.mock.ts',
   },
-  modulePaths: [...tsconfigModulePaths],
 };
