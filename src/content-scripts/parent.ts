038d20b32ba7d0565ffa7eae6d46206df356681f
Aaron Klinker <aaronklinker1@gmail.com>
2021-11-21 14:02:10 -0600

    inject anime skip website content scripts

--- src/content-scripts/parent.ts
+++ src/content-scripts/parent.ts
@@ -1,6 +1,7 @@
 import { ParentHosts } from '~/common/utils/CompileTimeConstants';
 import { loadedLog } from '~/common/utils/loadedLog';
 import { urlPatternMatch } from '~/common/utils/strings';
+import { initAnimeSkipParent } from './anime-skip';
 import { initCrunchyrollParent } from './services/crunchyroll/parent';
 import { initFunimation20210926Parent } from './services/funimation-2021-09-26/parent';
 import { initFunimationParent } from './services/funimation/parent';
@@ -8,6 +9,8 @@ import { initTestServiceParent } from './services/test-service/parent';
 import { initVrvParent } from './services/vrv/parent';
 
 const services: Record<string, () => void> = {
+  [ParentHosts.ANIME_SKIP]: initAnimeSkipParent,
+  [ParentHosts.ANIME_SKIP_WWW]: initAnimeSkipParent,
   [ParentHosts.CRUNCHYROLL_BETA]: initCrunchyrollParent,
   [ParentHosts.CRUNCHYROLL]: initCrunchyrollParent,
   [ParentHosts.FUNIMATION_20210926]: initFunimation20210926Parent,
