4e2439ca30723ef5c5998ccf589ea5045ae59dde
Aaron Klinker <aaronklinker1@gmail.com>
2021-11-29 19:53:40 -0600

    fix: don't count updates as installs
    Added a new metric to track updates

--- src/background/metrics.ts
+++ src/background/metrics.ts
@@ -3,6 +3,8 @@ import { detectBrowser } from '~/common/utils/browser';
 import { loadedLog, log } from '~/common/utils/log';
 import UsageStats from '~/common/utils/UsageStats';
 
+const EXTENSION_VERSION_STORAGE_KEY = 'extension-version';
+
 function getUninstallUrl(userId: string | undefined): string | undefined {
   if (userId == null) return undefined;
   const encodedUserId = encodeURIComponent(userId);
@@ -14,8 +16,18 @@ function getUninstallUrl(userId: string | undefined): string | undefined {
 export function initMetrics() {
   loadedLog('background/metrics.ts');
 
-  browser.runtime.onInstalled.addListener(() => {
-    UsageStats.saveEvent('extension_installed');
+  browser.runtime.onInstalled.addListener(async () => {
+    const result = await browser.storage.local.get(EXTENSION_VERSION_STORAGE_KEY);
+    const prevVersion = result[EXTENSION_VERSION_STORAGE_KEY] as string | undefined;
+    if (prevVersion == null) {
+      UsageStats.saveEvent('extension_installed');
+    } else if (prevVersion !== EXTENSION_VERSION) {
+      UsageStats.saveEvent('extension_updated', {
+        fromVersion: prevVersion,
+        toVersion: EXTENSION_VERSION,
+      });
+    }
+    await browser.storage.local.set({ [EXTENSION_VERSION_STORAGE_KEY]: EXTENSION_VERSION });
   });
 
   let prevUninstallUrl: string | undefined = undefined;
