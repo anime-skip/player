37b096de2714d4d08865237fa50190e4e568b7ca
Aaron Klinker <aaronklinker1@gmail.com>
2022-01-08 10:54:20 -0600

    chore: attempt to fix install metrics

--- src/background/metrics.ts
+++ src/background/metrics.ts
@@ -15,14 +15,20 @@ export function initMetrics() {
   loadedLog('background/metrics.ts');
 
   browser.runtime.onInstalled.addListener(event => {
+    log('onInstalled', event);
+    if (event.temporary) return;
+    console.log('onInstalled', event);
+
     if (event.reason === 'install') {
       UsageStats.saveEvent('extension_installed');
     } else if (event.reason === 'update') {
-      UsageStats.saveEvent('extension_updated', {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        fromVersion: event.previousVersion!,
-        toVersion: EXTENSION_VERSION,
-      });
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const fromVersion = event.previousVersion!;
+      const toVersion = EXTENSION_VERSION;
+
+      // Reloading temp extension triggers an "update" event with the same versions
+      if (fromVersion !== toVersion)
+        UsageStats.saveEvent('extension_updated', { fromVersion, toVersion });
     }
   });
 
