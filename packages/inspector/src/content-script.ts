0e606cac3202c222268b2f8d67de7388f29b4186
Aaron Klinker <aaronklinker1@gmail.com>
2022-05-06 21:33:59 -0500

    chore(inspector): update copied HTML prefix

--- packages/inspector/src/content-script.ts
+++ packages/inspector/src/content-script.ts
@@ -1,11 +1,15 @@
 import Browser from 'webextension-polyfill';
 
+function getHtml(url: string) {
+  const html = document.getElementsByTagName('html')[0].innerHTML;
+  return `<html>\n<!-- Copied via https://github.com/anime-skip/player/tree/main/packages/inspector -->\n<!-- ${url} -->\n${html}\n</html>`;
+}
+
 async function updateDom() {
   try {
     const url = window.location.href;
-    const html = document.getElementsByTagName('html')[0].innerHTML;
     console.log('[inspector] Updating HTML for', url);
-    await Browser.storage.local.set({ [url]: html });
+    await Browser.storage.local.set({ [url]: getHtml(url) });
   } catch (err) {
     console.error('[inspector]', err);
   }
