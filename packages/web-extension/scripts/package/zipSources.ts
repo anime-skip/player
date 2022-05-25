3d4b80a4501bdee0a4776b4060a7a1def140f72a
Aaron Klinker <aaronklinker1@gmail.com>
2022-05-25 07:52:07 +0300

    Remove private module downloads

--- packages/web-extension/scripts/package/zipSources.ts
+++ packages/web-extension/scripts/package/zipSources.ts
@@ -1,10 +1,7 @@
-import { execSync } from 'child_process';
 import fs from 'fs-extra';
 import path from 'path';
 import { zip } from 'zip-a-folder';
-import packageJson from '../../package.json';
 import { bash, BOLD, CODE, rootPath, run, subStep, title } from '../utils';
-import { downloadTarball } from './downloadTarball';
 
 /**
  * Files and folders relative to monorepo root to copy
@@ -60,41 +57,6 @@ export async function zipSources(OUTPUT_DIR: string) {
     }
   });
   subStep(`${BOLD}${SOURCES.join(' ')}`);
-
-  const privateDeps = Object.keys(packageJson.dependencies).filter(isPrivateDependency);
-  if (privateDeps.length > 0) {
-    await run(`Download private modules to ${CODE}local_modules/`, async () => {
-      const localModulesDir = path.resolve(tempFolder, 'local_modules');
-      fs.ensureDirSync(localModulesDir);
-      await Promise.all(
-        privateDeps.map(async dep => {
-          const url = execSync(`npm view ${dep} dist.tarball`, { encoding: 'utf-8' }).trim();
-          const filename = dependencyFilename(dep);
-          const tarballPath = path.resolve(localModulesDir, filename);
-          await downloadTarball(url, tarballPath);
-        })
-      );
-    });
-    subStep(`${BOLD}${privateDeps.join(' ')}`);
-
-    await run(`Pointing to local_modules in ${CODE}packages/web-extension/package.json`, () => {
-      const packageJsonCopy = JSON.parse(JSON.stringify(packageJson));
-      const depReplacer = (dep: string) => {
-        if (packageJsonCopy.dependencies[dep] != null) {
-          packageJsonCopy.dependencies[dep] = `../../local_modules/${dependencyFilename(dep)}`;
-        }
-        if (packageJsonCopy.devDependencies[dep] != null) {
-          packageJsonCopy.dependencies[dep] = `../../local_modules/${dependencyFilename(dep)}`;
-        }
-      };
-      privateDeps.forEach(depReplacer);
-      fs.writeFileSync(
-        path.join(tempFolder, 'packages/web-extension/package.json'),
-        JSON.stringify(packageJsonCopy, null, 2),
-        { encoding: 'utf-8' }
-      );
-    });
-  }
 
   await run(`Zipping ${CODE}sources.zip`, () => zip(tempFolder, sourcesZip));
 
