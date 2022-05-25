914195725e3299211f8e69c7b1092e6df3c46186
Aaron Klinker <aklinker1@users.noreply.github.com>
2022-05-25 08:29:03 +0300

    fix: use packages from NPM (#240)

--- packages/web-extension/scripts/package/zipSources.ts
+++ packages/web-extension/scripts/package/zipSources.ts
@@ -1,10 +1,7 @@
-import { execSync } from 'child_process';
 import fs from 'fs-extra';
 import path from 'path';
 import { zip } from 'zip-a-folder';
-import packageJson from '../../package.json';
 import { bash, BOLD, CODE, rootPath, run, subStep, title } from '../utils';
-import { downloadTarball } from './downloadTarball';
 
 /**
  * Files and folders relative to monorepo root to copy
@@ -27,13 +24,6 @@ const SOURCES = [
   'packages/web-extension/vite.config.ts',
   { in: 'packages/web-extension/SOURCE_CODE_REVIEW.md', out: 'SOURCE_CODE_REVIEW.md' },
 ];
-
-function isPrivateDependency(dependency: string) {
-  return dependency.startsWith('@anime-skip/');
-}
-function dependencyFilename(dependency: string) {
-  return dependency.replace(/@/g, '').replace(/\//, '-') + '.tgz';
-}
 
 export async function zipSources(OUTPUT_DIR: string) {
   title(`Creating ${CODE}sources.zip`);
@@ -60,41 +50,6 @@ export async function zipSources(OUTPUT_DIR: string) {
     }
   });
   subStep(`${BOLD}${SOURCES.join(' ')}`);
-
-  const privateDeps = Object.keys(packageJson.dependencies).filter(isPrivateDependency);
-  if (privateDeps.length > 0) {
-    await run(`Download private modules to ${CODE}local_modules/`, async () => {
-      const localModulesDir = path.resolve(tempFolder, 'local_modules');
-      fs.ensureDirSync(localModulesDir);
-      await Promise.all(
-        privateDeps.map(async dep => {
-          const url = execSync(`npm view ${dep} dist.tarball`, { encoding: 'utf-8' }).trim();
-          const filename = dependencyFilename(dep);
-          const tarballPath = path.resolve(localModulesDir, filename);
-          await downloadTarball(url, tarballPath);
-        })
-      );
-    });
-    subStep(`${BOLD}${privateDeps.join(' ')}`);
-
-    await run(`Pointing to local_modules in ${CODE}packages/web-extension/package.json`, () => {
-      const packageJsonCopy = JSON.parse(JSON.stringify(packageJson));
-      const depReplacer = (dep: string) => {
-        if (packageJsonCopy.dependencies[dep] != null) {
-          packageJsonCopy.dependencies[dep] = `../../local_modules/${dependencyFilename(dep)}`;
-        }
-        if (packageJsonCopy.devDependencies[dep] != null) {
-          packageJsonCopy.dependencies[dep] = `../../local_modules/${dependencyFilename(dep)}`;
-        }
-      };
-      privateDeps.forEach(depReplacer);
-      fs.writeFileSync(
-        path.join(tempFolder, 'packages/web-extension/package.json'),
-        JSON.stringify(packageJsonCopy, null, 2),
-        { encoding: 'utf-8' }
-      );
-    });
-  }
 
   await run(`Zipping ${CODE}sources.zip`, () => zip(tempFolder, sourcesZip));
 
--- packages/web-extension/scripts/zip-sources.ts
+++ packages/web-extension/scripts/zip-sources.ts
@@ -1,4 +1,3 @@
-import readline from 'readline';
 import { zipSources } from './package/zipSources';
 import { rootPath } from './utils';
 
@@ -6,18 +5,4 @@ async function zip() {
   zipSources(rootPath('artifacts')).catch(console.error);
 }
 
-if (!process.env.NODE_AUTH_TOKEN) {
-  const reader = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false,
-  });
-  process.stdout.write('From ~/.npmrc, enter your auth token:\nnpm.pkg.github.com/:_authToken=');
-  reader.on('line', line => {
-    process.env.NODE_AUTH_TOKEN = line;
-    zip();
-    reader.close();
-  });
-} else {
-  zip();
-}
+zip();
