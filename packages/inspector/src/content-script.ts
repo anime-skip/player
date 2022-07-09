2aae2ba725aa6b972f641f0c4363dd469fca2538
Aaron Klinker <aaronklinker1@gmail.com>
2022-07-08 19:00:52 -0500

    chore(inspector): detect when sites have iframes of the same URL
    Zoro was not returning the full HTML, and thus it was breaking

--- packages/inspector/src/content-script.ts
+++ packages/inspector/src/content-script.ts
@@ -1,13 +1,18 @@
 import Browser from 'webextension-polyfill';
 
 function getHtml(url: string) {
-  const html = document.getElementsByTagName('html')[0].innerHTML;
+  const html = document.body.outerHTML;
+  console.log(html.substring(0, 20));
+
   return `<html>\n<!-- Copied via https://github.com/anime-skip/player/tree/main/packages/inspector -->\n<!-- ${url} -->\n${html}\n</html>`;
 }
 
 async function updateDom() {
   try {
-    const url = window.location.href;
+    let url = window.location.href;
+    if (self !== top) {
+      url += '-iframe';
+    }
     console.log('[inspector] Updating HTML for', url);
     await Browser.storage.local.set({ [url]: getHtml(url) });
   } catch (err) {
@@ -15,5 +20,22 @@ async function updateDom() {
   }
 }
 
-updateDom();
-setInterval(updateDom, 1e3);
+const ignored = [
+  // Reduce zoro.to spam
+  'recaptcha',
+  'onetag',
+  'quantum',
+  'vlitag',
+  'addthis',
+  'prebid',
+  'adnxs',
+];
+
+try {
+  if (!ignored.find(i => location.href.includes(i))) {
+    updateDom();
+    setInterval(updateDom, 1e3);
+  }
+} catch (err) {
+  console.error(err);
+}
