e381437a7fd3c91997a5fee2eee3810328be8736
Aaron Klinker <aaronklinker1@gmail.com>
2022-09-02 12:16:25 -0500

    fix: "process is not defined" build errors

--- packages/web-extension/src/@types/shim-env.d.ts
+++ packages/web-extension/src/@types/shim-env.d.ts
@@ -1,11 +1,3 @@
-// Extend import.meta.env types
-interface ImportMetaEnv {
-  readonly VITE_API_ENV: 'prod' | 'test' | 'local' | undefined;
-}
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 // Extend process.env types
 namespace NodeJS {
   interface ProcessEnv extends [ImportMeta['env']] {
