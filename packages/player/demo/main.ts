17899b56992b10342a334e9456baf1162fa85b93
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-04-30 08:51:40 -0500

    chore: Demo episode info from env

--- packages/player/demo/main.ts
+++ packages/player/demo/main.ts
@@ -1,5 +1,24 @@
 import { createPlayer } from '../src';
 
+const getShowName = (): HTMLElement => document.getElementById('show')!;
+const getEpisodeName = (): HTMLElement => document.getElementById('episode')!;
+const getSeason = (): HTMLElement => document.getElementById('season')!;
+const getNumber = (): HTMLElement => document.getElementById('number')!;
+const getAbsoluteNumber = (): HTMLElement =>
+  document.getElementById('absolute-number')!;
+
+// Set the episode info from env
+if (import.meta.env.VITE_SHOW_NAME)
+  getShowName().textContent = import.meta.env.VITE_SHOW_NAME;
+if (import.meta.env.VITE_EPISODE_NAME)
+  getEpisodeName().textContent = import.meta.env.VITE_EPISODE_NAME;
+if (import.meta.env.VITE_SEASON)
+  getSeason().textContent = import.meta.env.VITE_SEASON;
+if (import.meta.env.VITE_NUMBER)
+  getNumber().textContent = import.meta.env.VITE_NUMBER;
+if (import.meta.env.VITE_ABSOLUTE_NUMBER)
+  getAbsoluteNumber().textContent = import.meta.env.VITE_ABSOLUTE_NUMBER;
+
 // Set a custom video for development from a .env file
 const videoUrl =
   import.meta.env.VITE_VIDEO_SRC ||
@@ -11,11 +30,11 @@ const player = createPlayer({
   serviceName: 'Development',
   getEpisodeInfo() {
     return {
-      showName: document.getElementById('show')?.textContent,
-      season: document.getElementById('season')?.textContent,
-      episodeName: document.getElementById('episode')?.textContent,
-      number: document.getElementById('number')?.textContent,
-      absoluteNumber: document.getElementById('absolute-number')?.textContent,
+      showName: getShowName().textContent,
+      episodeName: getEpisodeName().textContent,
+      season: getSeason().textContent,
+      number: getNumber().textContent,
+      absoluteNumber: getAbsoluteNumber().textContent,
     };
   },
   fullscreenElement: '.video-container',
