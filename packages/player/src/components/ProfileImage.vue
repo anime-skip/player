bfd87b5373677d943f1674450d484710d56b4576
Aaron Klinker <aklinker1@users.noreply.github.com>
2023-05-06 23:14:26 -0500

    chore: Default profile URL

--- packages/player/src/components/ProfileImage.vue
+++ packages/player/src/components/ProfileImage.vue
@@ -2,14 +2,22 @@
 import IconAccount from '~icons/anime-skip/account';
 
 const { state: auth } = useAuth();
+
+const defaultUrl =
+  'https://assets.aws.anime-skip.com/images/simple-profile/128.png';
 </script>
 
 <template>
-  <div class="flex bg-base-content rounded-full">
+  <div
+    class="flex rounded-full overflow-hidden"
+    :class="{
+      'bg-base-content': !auth,
+    }"
+  >
     <img
-      class="w-full h-full rounded-full overflow-hidden"
+      class="w-full h-full"
       v-if="auth?.account"
-      :src="auth.account.profileUrl"
+      :src="auth.account.profileUrl || defaultUrl"
     />
     <icon-account v-else class="m-auto" />
   </div>
