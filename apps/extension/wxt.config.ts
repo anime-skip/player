a70c02869a04cccc92cb49143c256ccd70fa8a79
Aaron Klinker <aaronklinker1@gmail.com>
2024-01-17 22:01:59 -0600

    fix: Set extension ID for firefox

--- apps/extension/wxt.config.ts
+++ apps/extension/wxt.config.ts
@@ -4,10 +4,24 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   srcDir: 'src',
   manifestVersion: 3,
-  manifest: ({ mode }) => ({
-    name: `Anime Skip Player${mode === 'development' ? ' (DEV)' : ''}`,
-    description: 'Custom video player for anime streaming websites.',
-    permissions: ['storage', 'tabs'],
-    host_permissions: ['<all_urls>'],
-  }),
+  manifest: ({ mode, browser }) => {
+    const manifest: any = {
+      name: `Anime Skip Player${mode === 'development' ? ' (DEV)' : ''}`,
+      description: 'Custom video player for anime streaming websites.',
+      permissions: ['storage', 'tabs'],
+      host_permissions: ['<all_urls>'],
+    };
+    if (browser === 'firefox') {
+      manifest.browser_specific_settings = {
+        gecko: {
+          id: '{0442d98f-4ecc-4859-a65a-13e5969da46b}',
+        },
+      };
+    }
+
+    return manifest;
+  },
+  zip: {
+    name: 'anime-skip-player',
+  },
 });
